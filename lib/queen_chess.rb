f804f35da11657efbc7abff5f6862cccb33313ce
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-12 16:28:01 -0700

    Update queen_attack? function to include returning true if diagonal

--- lib/queen_chess.rb
+++ lib/queen_chess.rb
@@ -4,6 +4,8 @@ class Array
       true
     elsif array[1] === self[1]
       true
+    elsif (array[0]-self[0]).abs === (array[1]-self[1]).abs
+      true
     else
       false
     end
--- spec/queen_chess_spec.rb
+++ spec/queen_chess_spec.rb
@@ -14,4 +14,15 @@ describe('Array#queen_attack?') do
   it('is true if the coordinates are horizontally in line with each other') do
     expect([1,3].queen_attack?([2, 3])).to(eq(true))
   end
+
+  it('is true if the coordinates are diagonally in line with each other') do
+    expect([3,3].queen_attack?([5, 5])).to(eq(true))
+    expect([3,3].queen_attack?([2, 4])).to(eq(true))
+    expect([3,3].queen_attack?([4, 2])).to(eq(true))
+    expect([3,3].queen_attack?([1, 1])).to(eq(true))
+    expect([3,3].queen_attack?([5, 1])).to(eq(true))
+    expect([3,3].queen_attack?([1, 5])).to(eq(true))
+    expect([3,3].queen_attack?([2, 2])).to(eq(true))
+    expect([3,3].queen_attack?([4, 4])).to(eq(true))
+  end
 end
