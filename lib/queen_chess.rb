1ff87054f7279012a2cb4a5099382bd5dd459a37
Dave Lindqvist <Lindqvist.Dave@gmail.com>
2021-07-12 16:20:07 -0700

    Update queen_attack? function to check if coordinates are horizontally in line with each other

--- lib/queen_chess.rb
+++ lib/queen_chess.rb
@@ -2,8 +2,12 @@ class Array
   def queen_attack?(array)
     if array[0] === self[0]
       true
+    elsif array[1] === self[1]
+      true
     else
       false
     end
   end
 end
+
+# row, column
--- spec/queen_chess_spec.rb
+++ spec/queen_chess_spec.rb
@@ -10,4 +10,8 @@ describe('Array#queen_attack?') do
   it('is true if the coordinates are vertically in line with each other') do
     expect([1,1].queen_attack?([1, 3])).to(eq(true))
   end
+
+  it('is true if the coordinates are horizontally in line with each other') do
+    expect([1,3].queen_attack?([2, 3])).to(eq(true))
+  end
 end
